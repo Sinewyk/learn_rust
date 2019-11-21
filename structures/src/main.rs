951c14a10da165cab6429d1d3e5eeed0f83b92b7
sinewyk <sinewyk@gmail.com>
2019-11-21 10:32:14 +0100

    refactor to struct

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -40,18 +40,28 @@ fn do_stuff_with_user() {
     println!("{}", user2);
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     // do_stuff_with_user();
 
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(width1, height1)
+        area(&rect1)
     );
+
+    println!("{:#?}", &rect1);
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
