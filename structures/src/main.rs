5e1242c015d47f76d89b3d12941b5bdac4efff80
sinewyk <sinewyk@gmail.com>
2019-11-21 10:41:22 +0100

    finished struct

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -46,6 +46,23 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     // do_stuff_with_user();
 
@@ -53,15 +70,21 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!(
+    /* println!(
         "The area of the rectangle is {} square pixels",
-        area(&rect1)
-    );
+        rect1.area()
+    ); */
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-    println!("{:#?}", &rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("{:#?}", Rectangle::square(1));
 }
