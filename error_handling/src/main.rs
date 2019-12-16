d180faa4fcbeb8b95b1360a03d41df7a293d7f0c
sinewyk <sinewyk@gmail.com>
2019-12-16 18:51:49 +0100

    started error handling

---
+++ error_handling/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    match File::open("hello.txt") {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    cleanup();
+
+    File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    cleanup();
+}
+
+fn cleanup() {
+    std::fs::remove_file("hello.txt").expect("Could not remove hello.txt");
+}
