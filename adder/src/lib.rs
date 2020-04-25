f333dcbb4b47c0afed99703b42930a7c26ab2a60
Sinewyk <sinewyk@gmail.com>
2020-04-25 17:10:40 +0200

    moar tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,15 +10,101 @@ impl Rectangle {
 	}
 }
 
+pub fn add_two(a: i32) -> i32 {
+	a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+	format!("Hello {}!", name)
+}
+
+pub struct Guess {
+	value: i32,
+}
+
+impl Guess {
+	pub fn new(value: i32) -> Guess {
+		if value < 1 {
+			panic!("Guess value must be >= 1, got {}", value);
+		} else if value > 100 {
+			panic!("Guess value must be <= 100, got {}", value);
+		}
+
+		Guess { value }
+	}
+}
+
 #[cfg(test)]
 mod tests {
+	use super::*;
+
 	#[test]
 	fn exploration() {
 		assert_eq!(2 + 2, 4);
 	}
 
 	#[test]
-	fn it_panics() {
-		panic!("Make this test fail!");
+	fn larget_can_hold_smaller() {
+		let larger = Rectangle {
+			width: 8,
+			height: 7,
+		};
+		let smaller = Rectangle {
+			width: 5,
+			height: 1,
+		};
+
+		assert!(larger.can_hold(&smaller));
+	}
+
+	#[test]
+	fn smaller_cannot_hold_larger() {
+		let larger = Rectangle {
+			width: 8,
+			height: 7,
+		};
+		let smaller = Rectangle {
+			width: 5,
+			height: 1,
+		};
+
+		assert!(!smaller.can_hold(&larger));
+	}
+
+	#[test]
+	fn it_adds_two() {
+		assert_eq!(4, add_two(2));
+	}
+
+	#[test]
+	fn greeting_contains_name() {
+		let result = greeting("Serge");
+		assert!(
+			result.contains("Serge"),
+			"Greeting did not contain name, value was `{}`",
+			result
+		);
+	}
+
+	#[test]
+	#[should_panic(expected = "Guess value must be <= 100")]
+	fn panics_if_higher_than_100() {
+		Guess::new(200);
+	}
+
+	#[test]
+	#[should_panic(expected = "Guess value must be >= 1")]
+	fn panics_if_lower_than_1() {
+		Guess::new(-1);
+	}
+
+	#[test]
+	#[ignore]
+	fn it_works() -> Result<(), String> {
+		if 2 + 2 == 4 {
+			Ok(())
+		} else {
+			Err(String::from("Not ok :/"))
+		}
 	}
 }
