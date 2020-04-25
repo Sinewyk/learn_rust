f01327b26976c804e34d58c540df6359cfb18c7a
Sinewyk <sinewyk@gmail.com>
2020-04-25 15:32:00 +0200

    finally, some hard fucking tabs

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,7 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+	width: u32,
+	height: u32,
+}
+
+impl Rectangle {
+	fn can_hold(&self, other: &Rectangle) -> bool {
+		self.width > other.width && self.height > other.height
+	}
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	#[test]
+	fn exploration() {
+		assert_eq!(2 + 2, 4);
+	}
+
+	#[test]
+	fn it_panics() {
+		panic!("Make this test fail!");
+	}
 }
