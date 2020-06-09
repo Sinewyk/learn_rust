a48e483f8c46640b252ba76668424a0b3a04de27
sinewyk <sinewyk@gmail.com>
2020-06-09 16:58:47 +0200

    finish separation main/lib

--- io_project_12/src/main.rs
+++ io_project_12/src/main.rs
@@ -1,5 +1,5 @@
+use io_project_12::Config;
 use std::env;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -16,25 +16,9 @@ fn main() {
 	println!("Searching for {}", config.query);
 	println!("In file {}", config.filename);
 
-	let contents =
-		fs::read_to_string(config.filename).expect("Something went wrong, could not read the file");
+	if let Err(e) = io_project_12::run(config) {
+		println!("Application error: {}", e);
 
-	println!("With text:\n{}", contents);
-}
-
-struct Config {
-	query: String,
-	filename: String,
-}
-
-impl Config {
-	fn new(args: &[String]) -> Result<Config, &'static str> {
-		if args.len() < 3 {
-			return Err("Not enough arguments, need 2 for query and filename");
-		}
-		let query = args[1].clone();
-		let filename = args[2].clone();
-
-		Ok(Config { query, filename })
-	}
+		process::exit(1);
+	};
 }
