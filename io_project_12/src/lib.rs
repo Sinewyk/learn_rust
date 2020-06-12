98e079a368079458bea718a755fdafac2060cd0a
sinewyk <sinewyk@gmail.com>
2020-06-12 22:12:13 +0200

    impl

--- io_project_12/src/lib.rs
+++ io_project_12/src/lib.rs
@@ -21,11 +21,23 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 	let contents = fs::read_to_string(config.filename)?;
 
+	let res = search(&config.query, &contents);
+
+	for match_ in res {
+		println!("{}", match_);
+	}
+
 	Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	vec![]
+	let mut res = vec![];
+	for line in contents.lines() {
+		if line.contains(query) {
+			res.push(line);
+		}
+	}
+	res
 }
 
 #[cfg(test)]
