23250437670629d22a5c506dd6698519639c36af
sinewyk <sinewyk@gmail.com>
2020-06-12 22:18:01 +0200

    impl insensitive search

--- io_project_12/src/lib.rs
+++ io_project_12/src/lib.rs
@@ -40,18 +40,48 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 	res
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+	let query = query.to_lowercase();
+
+	let mut res = vec![];
+
+	for line in contents.lines() {
+		if line.to_lowercase().contains(&query) {
+			res.push(line);
+		}
+	}
+
+	res
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
 
 	#[test]
-	fn one_result() {
+	fn case_sensitive() {
 		let query = "duct";
 		let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
 		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+	}
+
+	#[test]
+	fn case_isensitive() {
+		let query = "RuSt";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+		assert_eq!(
+			vec!["Rust:", "Trust me."],
+			search_case_insensitive(query, contents)
+		);
 	}
 }
