c00e09fc4907acc8500b91ae5a16b8157b6234b4
Sinewyk <sinewyk@gmail.com>
2020-06-19 22:44:59 +0200

    scope capturing example

--- closures_13/src/main.rs
+++ closures_13/src/main.rs
@@ -15,6 +15,14 @@ fn main() {
 	let v2 = c.value(2);
 
 	assert_ne!(v1, v2);
+
+	let x = 4;
+
+	let equal_to_x = |z| z == x;
+
+	let y = 4;
+
+	assert!(equal_to_x(y));
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
