2e72508e65ad4e66ed7123cb5a93f618236a82a1
sinewyk <sinewyk@gmail.com>
2020-06-28 19:20:16 +0200

    Rc, RefCell & stuff

--- pointers_15/src/main.rs
+++ pointers_15/src/main.rs
@@ -1,9 +1,21 @@
 use std::ops::Deref;
 
-pub enum List {
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List2 {
+	Cons2(Rc<RefCell<i32>>, Rc<List2>),
+	Nil2,
+}
+
+use crate::List2::{Cons2, Nil2};
+use std::cell::RefCell;
+
+/* enum List {
 	Cons(i32, Box<List>),
 	Nil,
-}
+} */
 
 // use crate::List::{Cons, Nil};
 
@@ -37,6 +49,34 @@ fn main() {
 	println!("created");
 	drop(c);
 	println!("eom");
+
+	let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+	println!("count after creating a = {}", Rc::strong_count(&a));
+	let b = Cons(3, Rc::clone(&a));
+	println!("count after creating b = {}", Rc::strong_count(&a));
+	{
+		let c = Cons(4, Rc::clone(&a));
+		println!("count after creating c = {}", Rc::strong_count(&a));
+	}
+	println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+	let value = Rc::new(RefCell::new(5));
+
+	let a = Rc::new(Cons2(Rc::clone(&value), Rc::new(Nil2)));
+
+	let b = Cons2(Rc::new(RefCell::new(6)), Rc::clone(&a));
+	let c = Cons2(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+	*value.borrow_mut() += 10;
+
+	println!("a after = {:?}", a);
+	println!("b after = {:?}", b);
+	println!("c after = {:?}", c);
+}
+
+enum List {
+	Cons(i32, Rc<List>),
+	Nil,
 }
 
 struct MyBox<T>(T);
