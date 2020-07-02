20347ad13dc1b465e7330ccd77ad6d81e3933904
Sinewyk <sinewyk@gmail.com>
2020-07-02 17:26:53 +0200

    channels

--- concurrency_16/src/main.rs
+++ concurrency_16/src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -14,6 +15,17 @@ fn main() {
 		println!("hi number {} from the main thread", i);
 		thread::sleep(Duration::from_millis(1));
 	}
+
+	let (tx, rx) = mpsc::channel();
+
+	thread::spawn(move || {
+		let val = String::from("hi");
+		tx.send(val).unwrap();
+	});
+
+	let received = rx.recv().unwrap();
+
+	println!("Got: {}", received);
 
 	// handle.join().unwrap();
 }
