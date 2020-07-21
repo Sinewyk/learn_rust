3f046e61c29eadf4b2c595fab1ef5aecbd14abae
sinewyk <sinewyk@gmail.com>
2020-07-21 18:36:39 +0200

    playing around, not exactly the example

--- final_20/src/main.rs
+++ final_20/src/main.rs
@@ -1,18 +1,20 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
+	let body = fs::read_to_string("hello.html").unwrap();
 	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
 
-		handle_connection(stream);
+		handle_connection(stream, &body);
 	}
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, body: &str) {
 	let mut buffer = [0; 1024];
 
 	stream.read(&mut buffer).unwrap();
@@ -20,5 +22,14 @@ fn handle_connection(mut stream: TcpStream) {
 	// buffer is [u8; _] ... should be something about the size of it
 	// and buffer[..] is [u8], should also be something about the size of it, the buffer itself
 	// knows how much it's been filled when writing into it ?
-	println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+	// println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+	let full_response = format!(
+		"HTTP/1.1 OK\r\nContent-Length: {}\r\n\r\n{}",
+		body.len(),
+		body
+	);
+
+	stream.write(full_response.as_bytes()).unwrap();
+	stream.flush().unwrap();
 }
