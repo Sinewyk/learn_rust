194e19c4bf2271741f59ee64584ac82321b234b6
sinewyk <sinewyk@gmail.com>
2020-07-21 19:25:11 +0200

    ok so far

--- final_20/src/main.rs
+++ final_20/src/main.rs
@@ -4,32 +4,31 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-	let body = fs::read_to_string("hello.html").unwrap();
 	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
 
-		handle_connection(stream, &body);
+		handle_connection(stream);
 	}
 }
 
-fn handle_connection(mut stream: TcpStream, body: &str) {
+fn handle_connection(mut stream: TcpStream) {
 	let mut buffer = [0; 1024];
-
 	stream.read(&mut buffer).unwrap();
 
-	// buffer is [u8; _] ... should be something about the size of it
-	// and buffer[..] is [u8], should also be something about the size of it, the buffer itself
-	// knows how much it's been filled when writing into it ?
-	// println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+	let get = b"GET / HTTP/1.1\r\n";
 
-	let full_response = format!(
-		"HTTP/1.1 OK\r\nContent-Length: {}\r\n\r\n{}",
-		body.len(),
-		body
-	);
+	let (status_line, filename) = if buffer.starts_with(get) {
+		("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+	} else {
+		("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+	};
 
-	stream.write(full_response.as_bytes()).unwrap();
+	let contents = fs::read_to_string(filename).unwrap();
+
+	let response = format!("{}{}", status_line, contents);
+
+	stream.write(response.as_bytes()).unwrap();
 	stream.flush().unwrap();
 }
